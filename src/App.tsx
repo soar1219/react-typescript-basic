7af3b7acf2d01b3c1de90a1ffee9219a6d2f1556
soar1219 <tsubasa1219.info@gmail.com>
2023-10-11 15:22:07 +0900

    バグを作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,27 @@
 import axios from "axios";
+import { useState } from "react";
+import styled from "styled-components";
+import { Todo } from "./Todo";
 
 function App() {
+  const [todos, setTodos] = useState<any>([]);
   const onClickFetchData = () => {
     axios.get("https://jsonplaceholder.typicode.com/todos").then((res) => {
-      console.log(res);
+      setTodos(res.data)
     })
   }
   return (
-    <div className="App">
+    <SContainer className="App">
       <button onClick={onClickFetchData}>データ取得</button>
-    </div>
+      {todos.map((todo)=> (
+        <Todo title={todo.title} userid={todo.userid}/>
+      ))}
+    </SContainer>
   );
 }
 
 export default App;
+
+const SContainer = styled.div`
+  
+`
