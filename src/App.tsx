f0693cf7c26250e985586d521483aa923f75e319
soar1219 <tsubasa1219.info@gmail.com>
2023-10-11 14:54:30 +0900

    事前準備完了

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
-import { Plactice1 } from './plactices/Plactice1';
-import { Plactice2 } from './plactices/Plactice2';
-import { Plactice3 } from './plactices/Plactice3';
-import { Plactice4 } from './plactices/Plactice4';
+import axios from "axios";
 
 function App() {
+  const onClickFetchData = () => {
+    axios.get("https://jsonplaceholder.typicode.com/todos").then((res) => {
+      console.log(res);
+    })
+  }
   return (
     <div className="App">
-      <Plactice1 />
-      <Plactice2 />
-      <Plactice3 />
-      <Plactice4 />
+      <button onClick={onClickFetchData}>データ取得</button>
     </div>
   );
 }
