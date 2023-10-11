04cd6d0704133dd5dc1b0254c95af1be219c10e4
soar1219 <tsubasa1219.info@gmail.com>
2023-10-11 17:31:53 +0900

    関数に型を指定する

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import styled from "styled-components";
 import { Todo } from "./Todo";
 import { TodoType } from "./types/todo";
+import { Text } from "./Text";
 
 function App() {
   const [todos, setTodos] = useState<Array<TodoType>>([]);
@@ -13,6 +14,7 @@ function App() {
   }
   return (
     <SContainer className="App">
+      <Text color="red" fontSize="18px" />
       <button onClick={onClickFetchData}>データ取得</button>
       {todos.map((todo)=> (
         <Todo key={todo.id} title={todo.title} userId={todo.userId} completed={todo.completed}/>
--- src/Todo.tsx
+++ src/Todo.tsx
@@ -1,8 +1,8 @@
+import { FC } from "react";
 import { TodoType } from "./types/todo";
 
 
-
-export const Todo = (props: Omit<TodoType, "id">) => { /* pickで抜き出しOmitで省略 */
+export const Todo: FC<Omit<TodoType, "id">> = (props) => { /* pickで抜き出しOmitで省略 */
     const{title, userId, completed = false /* 初期値を設定するといい */ } = props;
     const compleateMark = completed ? "[完]" : "[未]";
     return(
